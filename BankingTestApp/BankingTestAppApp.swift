1e20bf25fc42f937dea33a3e9802c9468a0f9479
Overlord <ssd@MacBook-Pro-SSD.local>
2024-05-28 14:15:16 +0300

    Initial commit

---
+++ BankingTestApp/BankingTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingTestAppApp.swift
+//  BankingTestApp
+//
+//  Created by Overlord on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BankingTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
